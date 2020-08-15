c543aaf2a081bf178c82da7bf1c22b4ba471480b
Chanon <pcg.mega@gmail.com>
2020-08-15 22:39:15 +0700

    edit

--- Week1-3/mean.cpp
+++ Week1-3/mean.cpp
@@ -2,14 +2,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 int main() {
-	int n{};
+	int n{}, count = 0;
 	double a{}, sum{}, avg{};
 	printf("Mean Calculator\nAmount Of Numbers : ");
 	scanf("%d", &n);
 
 	for (int i = 0;i < n;i++) {
-		printf("Enter the number : ");
-		scanf("%lf",&a);
+		count += 1;
+		printf("Enter #%d number : ", count);
+		scanf("%lf", &a);
 		sum += a;
 	}
 	avg = sum / n;
